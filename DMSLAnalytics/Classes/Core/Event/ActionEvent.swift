25280b23565be604aad0adf07a87e5beb9468f6e
Dmytro Shulzhenko <dmytro.shulzhenko@gen.tech>
2021-02-17 12:47:13 +0200

    0.3.9

--- DMSLAnalytics/Classes/Core/Event/ActionEvent.swift
+++ DMSLAnalytics/Classes/Core/Event/ActionEvent.swift
@@ -5,24 +5,21 @@
 //  Created by Dmytro Shulzhenko on 17.10.2020.
 //
 
-import UIKit
+import Foundation
 
 public struct ActionEvent: ParamsContainer {
     let name: String
     public var params: [String: Any]
     let isActive: Bool
     let isUrgent: Bool
-    let states: Set<UIApplication.State>
     
     public init(name: String,
                 params: [String: Any] = [:],
                 isActive: Bool = true,
-                isUrgent: Bool = false,
-                states: Set<UIApplication.State> = [.active, .inactive]) {
+                isUrgent: Bool = false) {
         self.name = name
         self.params = params
         self.isActive = isActive
         self.isUrgent = isUrgent
-        self.states = states
     }
 }
